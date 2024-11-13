499071ff0b7454ec72388ab428727eafb573437f
Farshad Karimi <krfarshad@gmail.com>
2024-11-13 10:45:39 +0330

    chore: add link to logo

--- client/src/components/Logo/index.tsx
+++ client/src/components/Logo/index.tsx
@@ -15,14 +15,16 @@ export const Logo = () => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
           ></path>
         </svg>
       </Link>
-      <h1 className="ml-2 text-2xl font-bold text-slate-400">MERN Chat</h1>
+      <Link href="/" className="ml-2 text-2xl font-bold text-slate-400">
+        MERN Chat
+      </Link>
     </>
   );
 };
