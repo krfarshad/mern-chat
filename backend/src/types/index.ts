07473283d88154cf4469fd420560f08c5db7810c
Farshad Karimi <krfarshad@gmail.com>
2024-11-14 18:34:10 +0330

    chore: add models types

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -1,28 +1,3 @@
-import { Dispatch, ReactNode, SetStateAction } from "react";
-
-export * from "./api";
-
-export interface ChildrenProps {
-  children: ReactNode;
-}
-
-export interface ChildrenOptionalProps {
-  children?: ReactNode;
-}
-
-export interface ClassNameProps {
-  className: string;
-}
-export interface ClassNameOptionalProps {
-  className?: string;
-}
-export type setStateProp<T> = Dispatch<SetStateAction<T>>;
-
-export type OptionProp = {
-  label: string;
-  value: any;
-};
-
 export type Message = {
   id: number;
   type: "action" | "text";
