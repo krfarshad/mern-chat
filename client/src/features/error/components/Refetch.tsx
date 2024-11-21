469440ce899f2eaefc444cd57f5759e7635ee635
Farshad Karimi <krfarshad@gmail.com>
2024-11-21 10:35:44 +0330

    style: manage refetch error component

--- client/src/features/error/components/Refetch.tsx
+++ client/src/features/error/components/Refetch.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@nextui-org/react";
+import { InfoCircle } from "iconsax-react";
+
 type Props = {
   onClick: any;
 };
 export const Refetch = (props: Props) => {
   const { onClick } = props;
-  return <div>Refetch</div>;
+  return (
+    <div className="w-full p-8 text-center">
+      <Button
+        color="primary"
+        onClick={onClick}
+        variant="shadow"
+        startContent={<InfoCircle size="20" color="#555" />}
+      >
+        Refetch again
+      </Button>
+    </div>
+  );
 };
