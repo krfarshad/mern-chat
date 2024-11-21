81b5b39d7ee336156b44da1901dfbe69143bf5dd
Farshad Karimi <krfarshad@gmail.com>
2024-11-21 09:58:26 +0330

    refactor: ui chose to start component

--- client/src/features/chat/components/templates/ChoseToStart.tsx
+++ client/src/features/chat/components/templates/ChoseToStart.tsx
@@ -1,3 +1,23 @@
+import Image from "next/image";
+import chose from "../../assets/chose.png";
+import StartChat from "../molecules/StartChat";
 export const ChoseToStart = () => {
-  return <div>Chose one of your chats</div>;
+  return (
+    <div className="flex h-full w-full ">
+      <div className="inset-0 m-auto text-center">
+        <Image
+          src={chose.src}
+          priority={false}
+          width={200}
+          height={200}
+          alt="chose a chat"
+          className="mx-auto opacity-80"
+        />
+        <p>You haven’t chosen a chat yet, or maybe you haven’t started one</p>
+        <div className="mt-6 w-full text-center">
+          <StartChat />
+        </div>
+      </div>
+    </div>
+  );
 };
